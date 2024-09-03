e85034ba03a81800026bcfb3171b369588c8b758
Sergey-py24 <chuvachoc2009@yandex.ru>
2024-09-03 18:56:26 +0300

    Add files via upload

---
+++ module_5_4.py
@@ -0,0 +1,31 @@
+
+class House:
+    constr_history = []
+
+    def __new__(cls, *args, **kwargs):
+        cls.constr_history.append(args[0])
+        return object.__new__(cls)
+
+    def __init__(self, name, num_of_floors: int):
+        if not isinstance(num_of_floors, int):
+            raise TypeError("ОШИБКА")
+        self.name = name
+        self.num_of_floors = num_of_floors
+
+    def __str__(self):
+        return self.name
+
+    def __del__(self):
+        print(f'{self.name} остался только в Википедии')
+
+
+H1 = House('ЖК Лето', 10)
+print(House.constr_history)
+H2 = House('ЖК Кремлевский', 20)
+print(House.constr_history)
+H3 = House('ЖК Старый',30)
+print(House.constr_history)
+del H2
+del H3
+print(House.constr_history)
+
